24e5c2cea32b66d7b6e8df5c694e6f6ed7f71a3a
AlexDavidSandoval <sandovalex2001.as@gmail.com>
2022-04-01 22:27:40 -0600

    LOGIN CON DISEÑO

--- PW2_IIIParcial_Grupo7/src/app.js
+++ PW2_IIIParcial_Grupo7/src/app.js
@@ -13,47 +13,46 @@ const rutaMarca = require('./rutas/rutaMarcas');
 const app = express();
 app.use('/producto/img', express.static(path.join(__dirname, 'public/img')));
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.engine('handlebars', engine({defaultLayout: 'main'}));
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', './src/views');
 
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('json spaces',2);
+app.set('json spaces', 2);
 
 app.use('/api/', require('./rutas/index'));
-app.use('/api/proveedores/',rutaProveedor);
-app.use('/api/departamentos/',rutaDepartamento);
-app.use('/api/compra/',rutaCompra);
-app.use('/api/detallecompra/',rutaDetalleCompra);
-app.use('/api/categoria/',rutaCategoria);
+app.use('/api/proveedores/', rutaProveedor);
+app.use('/api/departamentos/', rutaDepartamento);
+app.use('/api/compra/', rutaCompra);
+app.use('/api/detallecompra/', rutaDetalleCompra);
+app.use('/api/categoria/', rutaCategoria);
 app.use('/api/marca', rutaMarca);
-app.use('/img',express.static(__dirname+'/public/img'));
-app.use('/api/clientes/',require('./rutas/rutaclientes'));
-app.use('/api/direccionesEnvio/',require('./rutas/rutaDireccionesEnvio'));
-app.use('/api/usuarioCliente',require('./rutas/rutaUsuarioCliente'));
-app.use('/api/empleados/',require('./rutas/rutasEmpleados'));
-app.use('/api/usuarioempleados/',require('./rutas/rutasUsuarioEmpleados'));
-app.use('/api/rolesempleados/',require('./rutas/rutasRolesEmpleados'));
-app.use('/api/empresaenvios/',require('./rutas/rutasEmpresaEnvios'));
-app.use('/api/categoria/',require('./rutas/rutaCategoria'));
+app.use('/img', express.static(__dirname + '/public/img'));
+app.use('/api/clientes/', require('./rutas/rutaclientes'));
+app.use('/api/direccionesEnvio/', require('./rutas/rutaDireccionesEnvio'));
+app.use('/api/usuarioCliente', require('./rutas/rutaUsuarioCliente'));
+app.use('/api/empleados/', require('./rutas/rutasEmpleados'));
+app.use('/api/usuarioempleados/', require('./rutas/rutasUsuarioEmpleados'));
+app.use('/api/rolesempleados/', require('./rutas/rutasRolesEmpleados'));
+app.use('/api/empresaenvios/', require('./rutas/rutasEmpresaEnvios'));
+app.use('/api/categoria/', require('./rutas/rutaCategoria'));
 app.use('/api/marca', require('./rutas/rutaMarcas'));
 app.use('/api/envios', require('./rutas/rutaEnvios'));
 app.use('/api/imagen', require('./rutas/rutaImagen'));
 app.use('/api/productos', require('./rutas/rutaProductos'));
-app.use('/api/ciudades',require('./rutas/rutaCiudades'));
+app.use('/api/ciudades', require('./rutas/rutaCiudades'));
 //VENTAS
 app.use('/api/ventas', require('./rutas/rutasVentas'));
 
 //VENTAS DETALLE
 app.use('/api/ventasdetalle', require('./rutas/rutasDetalleVentas'));
-app.use('/api/carrito',rutaCarrito);
+app.use('/api/carrito', rutaCarrito);
 
 //definir el puerto que se usara en el servidor
-app.listen(6001, ()=>
-{
+app.listen(6001, () => {
     console.log("Servidor iniciado en el puerto 6001");
 });
