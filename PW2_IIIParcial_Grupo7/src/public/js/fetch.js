c393a504af85ae3d07d47b91b6cce135af3042e4
David Chavez <davidferchavez2001@gmail.com>
2022-04-03 13:54:30 -0600

    Boton agregar en la pagina de inicio

--- PW2_IIIParcial_Grupo7/src/public/js/fetch.js
+++ PW2_IIIParcial_Grupo7/src/public/js/fetch.js
@@ -9,10 +9,12 @@ const mostrarData = (data) =>{
     console.log(data);
     let body =''
     for(let i=0;i < data.length ;i++){
-        body += `<column class="column column--50 decoration" id="data">
+        body += `
+        <column class="column column--50 decoration" id="data">
             <img src="${data[i].imagen}" class="decoration__img"></img> 
             <div class="decoration__title">${data[i].descripcion_producto}</div>
             <div class="decoration__txt">${data[i].precio_actual}</div>
+            <input type="button" class="btn_agregar" value="Agregar"></input>
         </column>
         `
     }
